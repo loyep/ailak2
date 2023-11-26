65c4295b457ebd1a26b8d20110afa3ebc240b6f6
Zack <lorenx@163.com>
2023-11-27 00:20:31 +0800

    feat: update api

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,1 +1,12 @@
-export * from "./src";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api/v1");
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello Ai Lak!",
+  });
+});
+
+export const handler = handle(app);
---
+++ src/app/api/v1/[[...path]]/route.ts
@@ -0,0 +1,9 @@
+import { handler } from "@ailak/api";
+
+export {
+  handler as GET,
+  handler as POST,
+  handler as PUT,
+  handler as DELETE,
+  handler as PATCH,
+};
